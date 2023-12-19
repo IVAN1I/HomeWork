479020976eb8a49aac78f57cb52a37b0e68697c7
Ivan <snorloks99@gmail.com>
2023-12-19 13:16:21 +0100

    one tast

--- Cvest2.py
+++ Cvest2.py
@@ -1,8 +1,7 @@
 from pprint import pprint
-matrix = [[10,1,1,1,14],
-          [1,11,1,13,1],
-          [1,1,12,1, 1],
-          [1,11,1,13,1],
-          [10,1,1,1,14]
-          ]
+matrix = [[0] * 5 for _ in range (5)]
+for i in range(5):
+    matrix [i][i] = 10 + i
+for i in range(5):
+    matrix [i][4-i] = 14 - i
 pprint(matrix)
